c5c5bf6cfc50366ea531620ea9362fc87f3a80c9
lavander8 <lavander8@rambler.su>
2025-10-06 14:15:20 +0300

    Добавлена конструкция try-except

--- countdown.py
+++ countdown.py
@@ -1,5 +1,9 @@
-num = int(input("Введите число:\n"))
+try:
+    num = int(input("Введите число:\n"))
 
-while num > 0:
-    print(num)
-    num -= 1
+    while num > 0:
+        print(num)
+        num -= 1
+
+except ValueError:
+    print("Усп, вы ввели не число! Попробуйте заново.")
